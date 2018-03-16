6d903c43321253034b5fb9bef33a6a72db632116
Mike Harder <mharder@microsoft.com>
2018-03-16 11:47:58 -0700

    Bind kestrel to dynamic port 0 in a loop

---
+++ KestrelDynamicPort/IWebHostPortExtensions.cs
@@ -0,0 +1,32 @@
+﻿// Copied from https://github.com/aspnet/KestrelHttpServer/blob/dev/test/Kestrel.FunctionalTests/TestHelpers/IWebHostPortExtensions.cs
+
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+
+namespace Microsoft.AspNetCore.Hosting
+{
+    public static class IWebHostPortExtensions
+    {
+        public static int GetPort(this IWebHost host)
+        {
+            return host.GetPorts().First();
+        }
+
+        public static IEnumerable<int> GetPorts(this IWebHost host)
+        {
+            return host.GetUris()
+                .Select(u => u.Port);
+        }
+
+        public static IEnumerable<Uri> GetUris(this IWebHost host)
+        {
+            return host.ServerFeatures.Get<IServerAddressesFeature>().Addresses
+                .Select(a => new Uri(a));
+        }
+    }
+}
---
+++ KestrelDynamicPort/Program.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Threading;
+
+namespace KestrelDynamicPort
+{
+    class Program
+    {
+        private static readonly int _threads = Environment.ProcessorCount;
+        private static readonly Stopwatch _stopwatch = Stopwatch.StartNew();
+        private static readonly object _lock = new object();
+
+        static void Main(string[] args)
+        {
+            var targetPort = int.Parse(args[0]);
+
+            var threads = new Thread[_threads];
+            for (var i= 0; i < _threads; i++)
+            {
+                var j = i;
+                threads[i] = new Thread(() =>
+                {
+                    while (true)
+                    {
+                        TestPort(targetPort, j);
+                    }
+                });
+                threads[i].Start();
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+        }
+
+        private static void TestPort(int targetPort, int thread)
+        {
+            // Block all threads if target port has been hit
+            lock (_lock) { };
+
+            using (var host = CreateWebHost())
+            {
+                host.Start();
+
+                var port = host.GetPort();
+                Console.WriteLine($"[{_stopwatch.Elapsed}] [{thread}] {port} ");
+
+                if (port == targetPort)
+                {
+                    lock (_lock)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine($"Kestrel was assigned port {targetPort}.  Press Enter to continue...");
+                        Console.ReadLine();
+                    }
+                }
+            }
+        }
+
+        private static IWebHost CreateWebHost()
+        {
+            return new WebHostBuilder()
+                .UseKestrel(options =>
+                {
+                    options.Listen(IPAddress.Loopback, 0);
+                })
+                .Configure(app => app.Run(context =>
+                {
+                    return context.Response.WriteAsync("Hello World!");
+                }))
+                .Build();
+        }
+    }
+}
